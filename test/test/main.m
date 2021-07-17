47ae605ac72fcd691c432cc1e39f3b10a307736f
HaoQianbiao <464408508@qq.com>
2021-07-17 20:08:58 +0800

    First Commit

---
+++ test/test/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  test
+//
+//  Created by haoqianbiao on 2021/7/17.
+//  Copyright © 2021 haoqianbiao. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
